9e73ae92a32680a7e24f36e0205a246f6af6cf88
99MINTER <jeffreydrew449@gmail.com>
2025-12-04 11:17:28 +0100

    feat: scaffold of the 412 multimedia project group 1

---
+++ script.js
@@ -0,0 +1,21 @@
+tailwind.config = {
+    darkMode: "class",
+    theme: {
+        extend: {
+            colors: {
+                "primary": "#f20d33",
+                "background-light": "#f8f5f6",
+                "background-dark": "#221013",
+            },
+            fontFamily: {
+                "display": ["Lexend", "sans-serif"]
+            },
+            borderRadius: {
+                "DEFAULT": "0.25rem",
+                "lg": "0.5rem",
+                "xl": "0.75rem",
+                "full": "9999px"
+            },
+        },
+    },
+}
